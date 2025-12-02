21154592afa9323c8e3ee6cf0461a7d407a7da64
Aidan Farhi <aidanmcfarhi@gmail.com>
2025-12-02 11:07:01 -0500

    added 10 and 11

--- 10/exercise/src/script.js
+++ 10/exercise/src/script.js
@@ -6,21 +6,12 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
  */
 const loadingManager = new THREE.LoadingManager() 
 const textureLoader = new THREE.TextureLoader(loadingManager)
-const colorTexture = textureLoader.load('/textures/checkerboard-8x8.png')
+const colorTexture = textureLoader.load('/textures/minecraft.png')
+
 colorTexture.colorSpace = THREE.SRGBColorSpace
-// const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
-// const heightTexture = textureLoader.load('/textures/door/height.jpg')
-// const normalTexture = textureLoader.load('/textures/door/normal.jpg')
-// const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
-// const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
-// const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
-// colorTexture.repeat.x = 2
-// colorTexture.repeat.y = 3
-// colorTexture.wrapS = THREE.MirroredRepeatWrapping
-// colorTexture.wrapT = THREE.MirroredRepeatWrapping
-// colorTexture.offset.x = 0.5
-// colorTexture.offset.y = 0.5
-// colorTexture.rotation = Math.PI / 4
+
+colorTexture.generateMipmaps = false
+colorTexture.minFilter = THREE.NearestFilter
 colorTexture.magFilter = THREE.NearestFilter
 
 /**
